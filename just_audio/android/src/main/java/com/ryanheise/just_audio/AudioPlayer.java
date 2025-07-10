20e974f6e96ab7f38baf36b411e26b1d19bbceee
snipd-min <min@snipd.com>
2025-07-10 16:51:40 +0200

    Prevent audio effect issue from not allowing to play

--- just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
+++ just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
@@ -224,12 +224,17 @@ public class AudioPlayer implements MethodCallHandler, Player.Listener, Metadata
         if (this.audioSessionId != null) {
             for (Object rawAudioEffect : rawAudioEffects) {
                 Map<?, ?> json = (Map<?, ?>)rawAudioEffect;
-                AudioEffect audioEffect = decodeAudioEffect(rawAudioEffect, this.audioSessionId);
-                if ((Boolean)json.get("enabled")) {
-                    audioEffect.setEnabled(true);
+                try {
+                  AudioEffect audioEffect = decodeAudioEffect(rawAudioEffect, this.audioSessionId);
+                  if ((Boolean)json.get("enabled")) {
+                      audioEffect.setEnabled(true);
+                  }
+                  audioEffects.add(audioEffect);
+                  audioEffectsMap.put((String)json.get("type"), audioEffect);
+                } catch (Exception e) {
+                  e.printStackTrace();
+                  Log.e(TAG, "Error creating audio effect: " + json.get("type") + ", " + e.getMessage());
                 }
-                audioEffects.add(audioEffect);
-                audioEffectsMap.put((String)json.get("type"), audioEffect);
             }
         }
         enqueuePlaybackEvent();
@@ -820,7 +825,11 @@ public class AudioPlayer implements MethodCallHandler, Player.Listener, Metadata
     }
 
     private void audioEffectSetEnabled(String type, boolean enabled) {
+      if (audioEffectsMap.containsKey(type)) {
         audioEffectsMap.get(type).setEnabled(enabled);
+    } else {
+        Log.w(TAG, "AudioEffect of type " + type + " is not enabled, cannot set enabled state");
+    }
     }
 
     private void loudnessEnhancerSetTargetGain(double targetGain) {
